f197c3263fbddd7487cf7ccc7e5ddf3bc0438f68
pig1606 <pig1606@knu.ac.kr>
2022-05-21 00:51:43 +0900

    문서추가: xcode 파일을 생성했습니다.

---
+++ BasicCalculator/BasicCalculator/BasicCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicCalculatorApp.swift
+//  BasicCalculator
+//
+//  Created by Minyoung Kim on 2022/05/20.
+//
+
+import SwiftUI
+
+@main
+struct BasicCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BasicCalculator/BasicCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BasicCalculator
+//
+//  Created by Minyoung Kim on 2022/05/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
